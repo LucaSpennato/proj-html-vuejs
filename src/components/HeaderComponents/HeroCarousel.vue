2547db9592371b9d8147a22ca45225eec45a207c
LucaSpennato <luca.spennato@gmail.com>
2022-07-26 20:32:32 +0200

    styling carousel thumb, array in data added

--- src/components/HeaderComponents/HeroCarousel.vue
+++ src/components/HeaderComponents/HeroCarousel.vue
@@ -1,9 +1,18 @@
 <template>
   <section class="carousel-section container-fluid p-0">
     <HeaderNav/>
-    <div class="carousel text-center">
-      ciao
-    </div>
+    <div class="carousel flex-center  text-center">
+      <div class="container-small">
+        <div class="display-1 fw-bold mb-4">
+        ready team
+        </div>
+          <h5>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quas harum et, facilis obcaecati tempore aut facere, quis reiciendis inventore molestias exercitationem sed asperiores dolorum numquam fugit assumenda. Aliquam, sequi?
+          </h5>
+        </div>
+      </div>
+      <div class="lateral-thumb">
+      </div>
   </section>
 </template>
 
@@ -14,6 +23,36 @@ export default {
     name: 'HeroCarousel',
     components:{
         HeaderNav,
+    },
+    data: function(){
+      return{
+        carouselElements:[
+          {
+            title: 'example',
+            description: 'No matter what your company needs, we will be ready to assist you in the best possible way',
+            linkOne:{
+              url: '#',
+              text: 'get in touch',
+            },
+            linkTwo:{
+              url: '#',
+              text: 'read more',
+            }
+          },
+          {
+            title: 'ready team',
+            description: 'No matter what your company needs, we will be ready to assist you in the best possible way',
+            linkOne:{
+              url: '#',
+              text: 'get in touch',
+            },
+            linkTwo:{
+              url: '#',
+              text: 'read more',
+            }
+          },
+        ]
+      }
     }
 }
 </script>
@@ -24,15 +63,25 @@ export default {
     @import '../../scss/containers.scss';
     .carousel-section{
       height: 100vh;
-      // background-image: url('https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg');
       background-image: url('../../assets/backgrounds/bg-parallax.png'), 
                         // carousel placeholder, the og one file is corrupted
                         url('https://static.vecteezy.com/system/resources/previews/006/304/593/original/abstract-white-and-light-grey-geometric-square-overlapped-pattern-on-background-with-shadow-modern-silver-color-cube-shape-with-copy-space-simple-and-minimal-banner-design-eps10-vector.jpg');
       background-position: center;
-      // background-color: blue;
       .carousel{
-        height: 90%;
-      }
+        height: 80%;
+      };
+
+      .lateral-thumb::after{
+        content: '\00A0';
+        position: absolute;
+        height: 2.6rem;
+        width: .7rem;
+        border-radius: 20px;
+        border: 2px solid $CcBluelagoon;
+        // background-color: red;
+        right: .8rem;
+        top: 50%;
+      };
 
     }
 
