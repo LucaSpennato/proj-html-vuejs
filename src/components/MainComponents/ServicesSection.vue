f83b571237e4a91f63d003f70e5bd0f357dde517
LucaSpennato <luca.spennato@gmail.com>
2022-07-27 10:41:51 +0200

    signle card styled, medium container > 60%

--- src/components/MainComponents/ServicesSection.vue
+++ src/components/MainComponents/ServicesSection.vue
@@ -4,7 +4,7 @@
         <div class="row justify-content-between">
             <div class="col-8">
                 <div class="text-uppercase">
-                    our business area
+                    our business areas
                 </div>
                 <h1 class="fw-bold">
                     Exellence in <span class="company-styled-word">Services</span>
@@ -17,8 +17,14 @@
                 <a class="text-uppercase btn activeGreenLink px-4" @click.prevent href="#">see all</a>
             </div>
         </div>
-        <div class="row">
-            
+        <div class="row row-cols-3">
+            <div class="services-card col rounded p-5">
+                <div class="card-body">
+                    <i class="bi bi-airplane fs-1"></i>
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
         </div>    
     </div>
   </section>
@@ -34,6 +40,13 @@ export default {
     @import '../../scss/style.scss';
     #services{
         background-color: $ServicesBg;
+        padding: 8rem 0;
+
+        .services-card{
+            width: 20rem;
+            margin: .8rem;
+            background-color: $CcWhite;
+        }
     }
 
 </style>
