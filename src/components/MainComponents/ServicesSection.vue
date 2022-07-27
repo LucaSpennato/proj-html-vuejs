c3e6da59626a04b8f28e103e800eda01326c9dcb
LucaSpennato <luca.spennato@gmail.com>
2022-07-27 11:06:02 +0200

    card dinamically added, styled uptext

--- src/components/MainComponents/ServicesSection.vue
+++ src/components/MainComponents/ServicesSection.vue
@@ -6,7 +6,7 @@
                 <div class="text-uppercase">
                     our business areas
                 </div>
-                <h1 class="fw-bold">
+                <h1 class="fw-bold py-4">
                     Exellence in <span class="company-styled-word">Services</span>
                 </h1>
                 <p>
@@ -14,15 +14,17 @@
                 </p>
             </div>
             <div class="col-2 d-flex align-items-end justify-content-end">
-                <a class="text-uppercase btn activeGreenLink px-4" @click.prevent href="#">see all</a>
+                <a class="text-uppercase btn activeGreenLink px-4 mb-4" @click.prevent href="#">see all</a>
             </div>
         </div>
         <div class="row row-cols-3">
-            <div class="services-card col rounded p-5">
+            <div
+            v-for="(card, index) in servicesInfos" :key="index" 
+            class="services-card col rounded p-5">
                 <div class="card-body">
-                    <i class="bi bi-airplane fs-1"></i>
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <i :class="iconPath(card)" class="fs-1"></i>
+                    <h5 class="card-title text-capitalize py-3">{{ card.title }}</h5>
+                    <p class="card-text">{{ card.text }}</p>
                 </div>
             </div>
         </div>    
@@ -33,11 +35,16 @@
 <script>
 export default {
     name: 'ServicesSection',
+    methods:{
+        iconPath: function(object){
+            return 'bi bi-' + object.icon;
+        }
+    },
     data: function(){
         return{
             servicesInfos:[
                 {
-                    icon: 'network',
+                    icon: 'hdd-network',
                     title: 'audit & assurance',
                     text: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.',
                     url: '#',
@@ -85,10 +92,18 @@ export default {
         background-color: $ServicesBg;
         padding: 8rem 0;
 
+        p{
+            color: gray;
+        }
+
         .services-card{
             width: 20rem;
             margin: .8rem;
             background-color: $CcWhite;
+
+            i{
+                color: $CcBluelagoon;
+            }
         }
     }
 
