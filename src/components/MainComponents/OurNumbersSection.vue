7f52ecef3e625cbfd7ffbe58cd27a0d165d6e9e0
LucaSpennato <luca.spennato@gmail.com>
2022-08-05 12:47:25 +0200

    fix on col, spacing, sizes

--- src/components/MainComponents/OurNumbersSection.vue
+++ src/components/MainComponents/OurNumbersSection.vue
@@ -1,79 +1,78 @@
 <template>
   <section id="results-section">
     <div class="container flex-center">
-        <div class="container-medium">
-            <div class="row text-center">
+      <div class="row col-10 text-center justify-content-center">
 
-                <SectionsHeadings :info="titleInfo" />
-                
-                <div class="row mt-5 justify-content-between">
-                    <div class="result col-3 p-0" 
-                    v-for="(result, index) in companyResults" :key="index">
-                        <h2>{{ result.number }}</h2>
-                        <h3>{{ result.area }}</h3>
-                    </div>
-                </div>
+        <SectionsHeadings :info="titleInfo" />
 
-            </div>
+        <div class="result col-2 mx-4 mt-5"
+          v-for="(result, index) in companyResults"
+          :key="index">
+          <h2>{{ result.number }}</h2>
+          <p>{{ result.area }}</p>
         </div>
+
+      </div>
     </div>
-    </section>    
+  </section>
 </template>
 
 <script>
 import SectionsHeadings from "./SectionsHeadings.vue";
 
 export default {
-    name: 'OurNumbersSection',
-    components:{
-        SectionsHeadings,
-    },
-    data: function(){
-       return{
-            titleInfo: {
-                topSubtitle: "what are we doing",
-                title: "results in numbers",
-                description: false,
-                typeOne: false,
-                typeTwo: true,
-            },
-        companyResults:[
-            {
-                number: '128',
-                area: 'certifications',
-            },
-            {
-                number: '230',
-                area: 'employees',
-            },
-            {
-                number: '517',
-                area: 'customers',
-            },
-            {
-                number: '94',
-                area: 'countries served',
-            },
-        ],
-       }
-    }
-}
+  name: "OurNumbersSection",
+  components: {
+    SectionsHeadings,
+  },
+  data: function () {
+    return {
+      titleInfo: {
+        topSubtitle: "what are we doing",
+        title: "results in numbers",
+        description: false,
+        typeOne: false,
+        typeTwo: true,
+      },
+      companyResults: [
+        {
+          number: "128",
+          area: "certifications",
+        },
+        {
+          number: "230",
+          area: "employees",
+        },
+        {
+          number: "517",
+          area: "customers",
+        },
+        {
+          number: "94",
+          area: "countries served",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-    @import '../../scss/style.scss';
-    #results-section{
-        background-image: url('../../assets/backgrounds/bg-7.jpg');
-        padding: 6rem 0;
-        .result{
-            h2{
-                color: $brandTextColorLight;
+    @import "../../scss/style.scss";
+
+    #results-section {
+    background-image: url("../../assets/backgrounds/bg-7.jpg");
+    padding: 6rem 0;
+        .result {
+            h2 {
+            color: $brandTextColorLight;
+            font-weight: bolder;
             }
-            h3{
-                text-transform: capitalize;
-                color: $lightTextColor;
+            p {
+            text-transform: capitalize;
+            font-weight: bold;
+            color: $lightTextColor;
             }
         }
     }
-
 </style>
